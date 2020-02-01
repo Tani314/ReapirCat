4537c3fc8c67e37a48156fe66790df170d99fb75
Tani314 <tanitabassum299@gmail.com>
2020-02-01 17:51:30 -0500

    animation1
    not done yet

---
+++ LI_tools/Assets/Break.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            //destroyedVersion.SetActive(true);
+
+
+            //this.gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
