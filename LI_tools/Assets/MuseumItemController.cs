63bc3e6d96467dbfc7f017fe1552ccc41b2ac99e
ijl0322 <ijl0322@gmail.com>
2020-02-02 00:05:19 -0500

    Add music and rooms

--- LI_tools/Assets/MuseumItemController.cs
+++ LI_tools/Assets/MuseumItemController.cs
@@ -7,10 +7,12 @@ public class MuseumItemController : MonoBehaviour
     public GameObject completeObject;
     public GameObject brokenObject;
     public bool broken = false;
+
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,5 +24,6 @@ public class MuseumItemController : MonoBehaviour
     public void BreakIt() {
         completeObject.SetActive(false);
         brokenObject.SetActive(true);
+        audioSource.Play();
     }
 }
